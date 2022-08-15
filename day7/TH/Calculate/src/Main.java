659005508df2949ba94f88c424fd45ba90299db1
wjnzero <chuckieakenzzz@gmai.com>
2022-08-15 09:51:02 +0700

    day7

---
+++ day7/TH/Calculate/src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+        System.out.println(calculate(4,5,'-'));
+        System.out.println(calculate1(4,5,'-'));
+//        calculate(4,5,'-');
+    }
+
+    public static int calculate(int firstOperand, int secondOperand, char operator) {
+        switch (operator) {
+            case '+':
+                return firstOperand + secondOperand;
+            case '-':
+                return firstOperand - secondOperand;
+            case '*':
+                return firstOperand * secondOperand;
+            case '/':
+                if (secondOperand != 0)
+                    return firstOperand / secondOperand;
+                else
+                    throw new RuntimeException("Can not divide by 0");
+            default:
+                throw new RuntimeException("Unsupported operation");
+        }
+    }
+
+    public static final char ADDITION = '+';
+    public static final char SUBTRACTION = '-';
+    public static final char MULTIPLICATION = '*';
+    public static final char DIVISION = '/';
+
+    public static int calculate1(int firstOperand, int secondOperand, char operator) {
+        switch (operator) {
+            case ADDITION:
+                return firstOperand + secondOperand;
+            case SUBTRACTION:
+                return firstOperand - secondOperand;
+            case MULTIPLICATION:
+                return firstOperand * secondOperand;
+            case DIVISION:
+                if (secondOperand != 0)
+                    return firstOperand / secondOperand;
+                else
+                    throw new RuntimeException("Can not divide by 0");
+            default:
+                throw new RuntimeException("Unsupported operation");
+        }
+    }
+
+
+}
+
